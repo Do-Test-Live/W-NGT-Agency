418efebd2cf0044bb0eaa8b3d0c830e693471e4a
Monoget <monoget2@gmail.com>
2023-02-26 13:26:03 +0600

    2/26/2023

--- contact.php
+++ contact.php
@@ -16,101 +16,132 @@
     <?php include('includes/sticky.php'); ?>
     <!-- End Sticky Header ~-->
 
-        <!-- Start Frontpage Banner Section -->
-        <div class="frontpage-banner-section frontpage-banner-style-three page-inner-block pd-b-200">
-            <div class="element-group">
-                <div class="element one"></div>
-                <div class="element two"></div>
-            </div>
-            <div class="container">
-                <div class="row align-items-center">
-                    <div class="col-lg-10 col-md-10">
-                        <div class="frontpage-banner-content">
-                            <h2 class="hero-title w-200 aos-item mrb-0" data-aos="fade-left" data-aos-duration="100" data-aos-once="true">Contact Us</h2>
-                            <!-- /.hero-title -->
-                        </div><!-- /.hero-content-area -->
-                    </div>
+
+    <!-- Start Frontpage Banner Section -->
+    <div class="frontpage-banner-section frontpage-banner-style-three page-inner-block">
+        <div class="element-group">
+            <div class="element one"></div>
+            <div class="element two"></div>
+        </div>
+        <div class="container">
+            <div class="row align-items-center">
+                <div class="col-lg-5 col-md-6">
+                    <div class="frontpage-banner-content">
+                        <h2 class="hero-title w-800 aos-item" data-aos="fade-left" data-aos-duration="300"
+                            data-aos-once="true">Contact Us</h2>
+                        <!-- /.hero-title -->
+                        <div class="hero-desc aos-item" data-aos="fade-left" data-aos-duration="400"
+                             data-aos-once="true">
+                            <!--<p>Entrepreneurship requires concentration, why waste time on trivial administrative affairs? Your energy should be invested in your expertise, and trivial and complicated matters can be trusted to our professional team. Pioneer cattle can complete the tasks you entrusted for you faster and more accurately, so that you have no worries.</p>-->
+                        </div><!-- /.hero-desc -->
+                    </div><!-- /.hero-content-area -->
                 </div>
+                <div class="col-lg-6 offset-lg-1 col-md-6">
+                    <div class="row align-items-center">
+                        <div class="col-lg-5 col-6">
+                            <div class="row">
+                                <div class="col-lg-12 col-md-12">
+                                    <img class="radius-4 aos-item" data-aos="fade-right" data-aos-duration="500"
+                                         data-aos-once="true" src="assets/images/about/about-inner-2.jpg" alt="Thumb">
+                                </div>
+                                <!-- /.col-lg-12 col-md-12 -->
+                            </div>
+                        </div>
+                        <div class="col-lg-7 col-6">
+                            <img class="radius-4 aos-item" data-aos="fade-left" data-aos-duration="600"
+                                 data-aos-once="true" src="assets/images/about/about-inner-1.jpg" alt="Thumb">
+                        </div>
+                        <!-- /.col-lg-7 col-6 -->
+                    </div>
+
+                </div><!-- /.col-lg-6 -->
             </div>
         </div>
-        <!-- End Frontpage Banner Section -->
-
-        <!-- Start Contact Block -->
-        <div class="contact-form-block style-two style-three ptb-130">
-            <ul class="particle-shapes">
-				<li class="dot-shape shape-one"></li>
-				<li class="dot-shape shape-two"></li>
-				<li class="dot-shape shape-three"></li>
-				<li class="dot-shape shape-four"></li>
-				<li class="dot-shape shape-five"></li>
-			</ul>
-			<div class="container ml-b-40">
-				<div class="row align-items-center">
-					<div class="col-lg-8 col-md-8">
-						<div class="contact-form-area">
-							<form id="contact_form" class="contact-form" action="http://www.incognitothemes.com/jura/html/assets/php/contact.php">
-								<div class="row">
-									<div class="col-lg-12 col-md-12">
-										<div class="form-group">
-											<label for="name">Your Name </label>
-											<input class="form-controller" id="name" name="name" type="text">
-										</div>
-									</div>
-									<!--./ col-lg-6-->
-									<div class="col-lg-12 col-md-12">
-										<div class="form-group">
-											<label for="phone">Your Phone</label>
-											<input class="form-controller" id="number" name="phone" type="number">
-										</div>
-									</div>
-                                    <div class="col-lg-12 col-md-12">
-                                        <div class="form-group">
-                                            <label for="email">Your Email </label>
-                                            <input class="form-controller" id="email" name="email" type="email">
-                                        </div>
+    </div>
+    <!-- End Frontpage Banner Section -->
+    <!-- Start Contact Block -->
+    <div class="contact-form-block style-two style-three ptb-130">
+        <ul class="particle-shapes">
+            <li class="dot-shape shape-one"></li>
+            <li class="dot-shape shape-two"></li>
+            <li class="dot-shape shape-three"></li>
+            <li class="dot-shape shape-four"></li>
+            <li class="dot-shape shape-five"></li>
+        </ul>
+        <div class="container ml-b-40">
+            <div class="row align-items-center">
+                <div class="col-lg-8 col-md-8">
+                    <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d652.5998004707865!2d114.19769630194097!3d22.29079165782701!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x34040104842fb5f7%3A0xe8e1395df200e15d!2sMax%20Share%20Centre!5e0!3m2!1sen!2sbd!4v1677394950081!5m2!1sen!2sbd"
+                            width="800" height="600" style="border:0; margin-bottom: 100px;" allowfullscreen="" loading="lazy"
+                            referrerpolicy="no-referrer-when-downgrade"></iframe>
+                    <div class="contact-form-area">
+                        <form id="contact_form" class="contact-form"
+                              action="http://www.incognitothemes.com/jura/html/assets/php/contact.php">
+                            <div class="row">
+                                <div class="col-lg-12 col-md-12">
+                                    <div class="form-group">
+                                        <label for="name">Your Name </label>
+                                        <input class="form-controller" id="name" name="name" type="text">
                                     </div>
-									<!--./ col-lg-6 -->
-									<div class="col-12">
-										<div class="form-group">
-											<label for="name">Your Message </label>
-											<textarea id="message" name="message" class="form-controller" rows="4"
-												cols="50"></textarea>
-										</div>
-									</div><!-- /.col-12 -->
-									<div class="col-12 mrt-15">
-										<button type="submit" class="btn btn-primary rounded-pill btn-gradient-three">Submit</button>
-									</div>
-									<!--./ col-lg-6 -->
-								</div><!-- /.row -->
-							</form><!-- /.contact-form -->
-						</div><!-- /.contact-form-area -->
-					</div><!-- /.col-lg-8 -->
-                    <div class="col-lg-3 offset-lg-1 col-md-3">
-                        <div class="single-contact-info">
-                            <h3 class="title">Contacts</h3>
-                            <div class="card-info">
-                                <ul class="info-list">
-                                    <li><i class="icofont-phone"></i> +852 3895 5666</li>
-									<li><i class="icofont-send-mail"></i> <a href="#">info@example.com</a></li>
-                                </ul>
-                            </div><!-- /.card-info -->
-                        </div><!-- /.single-contact-info -->
-                        <div class="single-contact-info">
-                            <h3 class="title">Address</h3>
-                            <div class="card-info">
-                                <h5>North Point</h5>
-                                <p>Room 19A, Shangrun Centre, 367-373 King's Road, North Point, Hong Kong</p>
-                                <P>Located on King's Road, in the heart of North Point</P>
-                                <P>Only a 5-minute walk from MTR North Point Station</P>
-                                <P>Adjacent to bus stops and tram stops, it takes half an hour to reach various districts in Hong Kong, Kowloon and the New Territories</P>
-                                <P>Complete banking network, most banks are within a few steps</P>
-                            </div><!-- /.card-info -->
-                        </div><!-- /.single-contact-info -->
-                    </div>
-				</div><!-- /.row -->
-			</div><!-- /.container -->
-		</div>
-        <!--~~./ End Contact Block ~~-->
+                                </div>
+                                <!--./ col-lg-6-->
+                                <div class="col-lg-12 col-md-12">
+                                    <div class="form-group">
+                                        <label for="phone">Your Phone</label>
+                                        <input class="form-controller" id="number" name="phone" type="number">
+                                    </div>
+                                </div>
+                                <div class="col-lg-12 col-md-12">
+                                    <div class="form-group">
+                                        <label for="email">Your Email </label>
+                                        <input class="form-controller" id="email" name="email" type="email">
+                                    </div>
+                                </div>
+                                <!--./ col-lg-6 -->
+                                <div class="col-12">
+                                    <div class="form-group">
+                                        <label for="name">Your Message </label>
+                                        <textarea id="message" name="message" class="form-controller" rows="4"
+                                                  cols="50"></textarea>
+                                    </div>
+                                </div><!-- /.col-12 -->
+                                <div class="col-12 mrt-15">
+                                    <button type="submit" class="btn btn-primary rounded-pill btn-gradient-three">
+                                        Submit
+                                    </button>
+                                </div>
+                                <!--./ col-lg-6 -->
+                            </div><!-- /.row -->
+                        </form><!-- /.contact-form -->
+                    </div><!-- /.contact-form-area -->
+                </div><!-- /.col-lg-8 -->
+                <div class="col-lg-3 offset-lg-1 col-md-3">
+                    <div class="single-contact-info">
+                        <h3 class="title">Contacts</h3>
+                        <div class="card-info">
+                            <ul class="info-list">
+                                <li><i class="icofont-phone"></i> +852 3895 5666</li>
+                                <li><i class="icofont-send-mail"></i> <a href="#">info@example.com</a></li>
+                            </ul>
+                        </div><!-- /.card-info -->
+                    </div><!-- /.single-contact-info -->
+                    <div class="single-contact-info">
+                        <h3 class="title">Address</h3>
+                        <div class="card-info">
+                            <h5>North Point</h5>
+                            <p>Room 19A, Shangrun Centre, 367-373 King's Road, North Point, Hong Kong</p>
+                            <P>Located on King's Road, in the heart of North Point</P>
+                            <P>Only a 5-minute walk from MTR North Point Station</P>
+                            <P>Adjacent to bus stops and tram stops, it takes half an hour to reach various districts in
+                                Hong Kong, Kowloon and the New Territories</P>
+                            <P>Complete banking network, most banks are within a few steps</P>
+                        </div><!-- /.card-info -->
+                    </div><!-- /.single-contact-info -->
+                </div>
+            </div><!-- /.row -->
+        </div><!-- /.container -->
+    </div>
+    <!--~~./ End Contact Block ~~-->
 
     <!-- Start Site Footer -->
     <?php include('includes/footer.php'); ?>
---
+++ service.php
@@ -0,0 +1,207 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <?php include('includes/css.php'); ?>
+</head>
+
+<body data-spy="scroll" data-offset="170" data-target=".navigation-area">
+
+<div class="page-wrapper">
+    <!-- Header -->
+    <?php include('includes/header.php'); ?>
+    <!-- End Site Header -->
+    <!-- Sticky Header -->
+    <?php include('includes/sticky.php'); ?>
+    <!-- End Sticky Header ~-->
+
+
+    <!-- Start Frontpage Banner Section -->
+    <div class="frontpage-banner-section frontpage-banner-style-three page-inner-block">
+        <div class="element-group">
+            <div class="element one"></div>
+            <div class="element two"></div>
+        </div>
+        <div class="container">
+            <div class="row align-items-center">
+                <div class="col-lg-5 col-md-6">
+                    <div class="frontpage-banner-content">
+                        <h2 class="hero-title w-800 aos-item" data-aos="fade-left" data-aos-duration="300"
+                            data-aos-once="true">Contact Us</h2>
+                        <!-- /.hero-title -->
+                        <div class="hero-desc aos-item" data-aos="fade-left" data-aos-duration="400"
+                             data-aos-once="true">
+                            <!--<p>Entrepreneurship requires concentration, why waste time on trivial administrative affairs? Your energy should be invested in your expertise, and trivial and complicated matters can be trusted to our professional team. Pioneer cattle can complete the tasks you entrusted for you faster and more accurately, so that you have no worries.</p>-->
+                        </div><!-- /.hero-desc -->
+                    </div><!-- /.hero-content-area -->
+                </div>
+                <div class="col-lg-6 offset-lg-1 col-md-6">
+                    <div class="row align-items-center">
+                        <div class="col-lg-5 col-6">
+                            <div class="row">
+                                <div class="col-lg-12 col-md-12">
+                                    <img class="radius-4 aos-item" data-aos="fade-right" data-aos-duration="500"
+                                         data-aos-once="true" src="assets/images/about/about-inner-2.jpg" alt="Thumb">
+                                </div>
+                                <!-- /.col-lg-12 col-md-12 -->
+                            </div>
+                        </div>
+                        <div class="col-lg-7 col-6">
+                            <img class="radius-4 aos-item" data-aos="fade-left" data-aos-duration="600"
+                                 data-aos-once="true" src="assets/images/about/about-inner-1.jpg" alt="Thumb">
+                        </div>
+                        <!-- /.col-lg-7 col-6 -->
+                    </div>
+
+                </div><!-- /.col-lg-6 -->
+            </div>
+        </div>
+    </div>
+    <!-- End Frontpage Banner Section -->
+
+        <!-- Start Services Block -->
+        <div class="services-block style-two ptb-130">
+            <div class="container ml-b-30">
+                <div class="row">
+                    <div class="col-lg-4 col-md-6 aos-item" data-aos="fade-up" data-aos-duration="250" data-aos-once="true">
+                        <div class="service-item style-three flex radius-2">
+                            <div class="service-icon mrr-30">
+                                <i class="icofont-cube"></i>
+                            </div>
+                            <!-- /.service-icon -->
+                            <div class="service-info">
+                                <h6 class="title mrb-5">UI Design</h6>
+                                <p>
+                                    User interface design or user interface engineering.
+                                </p>
+                            </div>
+                            <!-- /.service-info -->
+                        </div>
+                        <!-- /.service-item -->
+                    </div>
+                    <!-- /.col-lg-6 -->
+                    <div class="col-lg-4 col-md-6 aos-item" data-aos="fade-up" data-aos-duration="500" data-aos-once="true">
+                        <div class="service-item style-three flex radius-2">
+                            <div class="service-icon mrr-30">
+                                <i class="icofont-light-bulb"></i>
+                            </div>
+                            <!-- /.service-icon -->
+                            <div class="service-info">
+                                <h3 class="title mrb-5">Product Design</h3>
+                                <p>
+                                    Product design as a verb is to create a new product.
+                                </p>
+                            </div>
+                            <!-- /.service-info -->
+                        </div>
+                        <!-- /.service-item -->
+                    </div>
+                    <!-- /.col-lg-6 -->
+                    <div class="col-lg-4 col-md-6 aos-item" data-aos="fade-up" data-aos-duration="750" data-aos-once="true">
+                        <div class="service-item style-three flex radius-2">
+                            <div class="service-icon mrr-30">
+                                <i class="icofont-bell"></i>
+                            </div>
+                            <!-- /.service-icon -->
+                            <div class="service-info">
+                                <h3 class="title mrb-5">App Design</h3>
+                                <p>
+                                    App design combines the user interface.
+                                </p>
+                            </div>
+                            <!-- /.service-info -->
+                        </div>
+                        <!-- /.service-item -->
+                    </div>
+                    <!-- /.col-lg-6 -->
+                </div>
+                <div class="row">
+                    <div class="col-lg-4 col-md-6 aos-item" data-aos="fade-up" data-aos-duration="500" data-aos-once="true">
+                        <div class="service-item style-three flex radius-2">
+                            <div class="service-icon mrr-30">
+                                <i class="icofont-chart-histogram"></i>
+                            </div>
+                            <!-- /.service-icon -->
+                            <div class="service-info">
+                                <h6 class="title mrb-5">Digital Markerting</h6>
+                                <p>
+                                    User interface design or user interface engineering.
+                                </p>
+                            </div>
+                            <!-- /.service-info -->
+                        </div>
+                        <!-- /.service-item -->
+                    </div>
+                    <!-- /.col-lg-6 -->
+                    <div class="col-lg-4 col-md-6 aos-item" data-aos="fade-up" data-aos-duration="600" data-aos-once="true">
+                        <div class="service-item style-three flex radius-2">
+                            <div class="service-icon mrr-30">
+                                <i class="icofont-video-cam"></i>
+                            </div>
+                            <!-- /.service-icon -->
+                            <div class="service-info">
+                                <h3 class="title mrb-5">Production Services</h3>
+                                <p>
+                                    Product design as a verb is to create a new product.
+                                </p>
+                            </div>
+                            <!-- /.service-info -->
+                        </div>
+                        <!-- /.service-item -->
+                    </div>
+                    <!-- /.col-lg-6 -->
+                    <div class="col-lg-4 col-md-6 aos-item" data-aos="fade-up" data-aos-duration="700" data-aos-once="true">
+                        <div class="service-item style-three flex radius-2">
+                            <div class="service-icon mrr-30">
+                                <i class="icofont-iphone"></i>
+                            </div>
+                            <!-- /.service-icon -->
+                            <div class="service-info">
+                                <h3 class="title mrb-5">Mobile App</h3>
+                                <p>
+                                    App design combines the user interface.
+                                </p>
+                            </div>
+                            <!-- /.service-info -->
+                        </div>
+                        <!-- /.service-item -->
+                    </div>
+                    <!-- /.col-lg-6 -->
+                </div>
+            </div>
+            <!-- /.container -->
+        </div>
+        <!--~~./ End Services Block ~~-->
+    <!-- Start Call to Action Block -->
+        <section class="section-call-to-action style-four ptb-130 bg-primary" style="margin-bottom: -50px;">
+            <div class="container">
+                <div class="row justify-content-center">
+                    <div class="col-md-12">
+                        <div class="text-feature-block ms-mrb-50 text-center">
+                            <h4 class="sub-title color-secondary aos-item" data-aos="fade-up" data-aos-duration="300" data-aos-once="true">Grow Your Business</h4>
+                            <h2 class="title color-white aos-item" data-aos="fade-up" data-aos-duration="400" data-aos-once="true">Ready to start scaling<br> your business now?</h2>
+                            <h5 class="description color-white aos-item" data-aos="fade-up" data-aos-duration="500" data-aos-once="true">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nam volutpat imperdiet turpis, ut tincidunt ipsum. Ut dictum bibendum</h5>
+                        </div>
+                        <!-- /.section-title -->
+                    </div>
+                </div>
+                <div class="row mrt-80">
+                    <div class="col-md-12 text-center aos-item" data-aos="fade-up" data-aos-duration="600" data-aos-once="true">
+                        <a class="btn btn-primary btn-lg radius-2 btn-black" href="contact.php">Service</a>
+                    </div>
+                </div>
+            </div>
+        </section>
+        <!--~~./ End Call to Action Block ~~-->
+
+    <!-- Start Site Footer -->
+    <?php include('includes/footer.php'); ?>
+    <!--~./ End Site Footer ~-->
+</div>
+<!--~~./ End Page Wrapper ~~-->
+
+<!-- All The JS Files  -->
+<?php include('includes/js.php'); ?>
+</body>
+
+</html>
